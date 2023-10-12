cee8a85a11c06b0519b71e47f31f5f5602e195b9
Erimed33 <erikamedina@pursuit.org>
2023-10-12 17:46:38 -0400

    last-fuct-edit

--- index.js
+++ index.js
@@ -80,7 +80,13 @@ groupedClasses[instructor].push(yogaClass)
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects, each one without the age field
  */
-function omitAgeFromMembers(collection) {}
+function omitAgeFromMembers(collection) {
+  return collection.map((member) => {
+  
+    const { age, ...memberWithoutAge } = member;
+    return memberWithoutAge;
+  });
+}
 
 /**
  * Return the count of the number of classes a particular instructor teaches
@@ -88,21 +94,62 @@ function omitAgeFromMembers(collection) {}
  * @param {String} instructorName - The name of the instructor
  * @return {number} The sum of the numbers in an array
  */
-function countClassesByInstructor(collection, instructor) {}
+function countClassesByInstructor(collection, instructor) {
+  let classCount = 0
+  
+  // loop through the collection of yoga classes
+  for (const yogaClass of collection) {
+   
+    // check current class, compare instructor
+    if (yogaClass.instructor === instructor) {
+      // if matchs up, increment the classCount
+      classCount++;
+    }  
+  }
+  if (classCount === 0 ) {
+    return 'There is no instructor by that name.'
+  }
+  return classCount;
+}
 
 /**
  * Remove inactive members from the members array
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects with only active members
  */
-function removeInactiveMembers(collection) {}
+function removeInactiveMembers(collection) {
+  console.log(collection
+    )
+
+  const activeMembers = collection.filter((member) => member.currentMember === true);
+
+  return activeMembers;
+}
 
 /**
  * Get a list of unique class titles and their price
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that have a unique title and a price
  */
-function getUniqueClasses(collection) {}
+function getUniqueClasses(collection) {
+  let uniqueClass = {}
+  
+
+  collection.forEach((yogaClass) => {
+    const price = yogaClass.priceInCents
+    const title = yogaClass.title 
+
+    if (!uniqueClass[title] || price < uniqueClass[title].price) {
+      uniqueClass[title] = { title, priceInCents: price};
+    }
+  });
+
+
+  const uniqueClassesArray = Object.values(uniqueClass);
+
+  return uniqueClassesArray;
+
+}
 
 /**
  * Get a list of classes organized by title, then by level.
@@ -110,7 +157,35 @@ function getUniqueClasses(collection) {}
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that are organized by title then by level. The array should only have the title, instructor, and level fields
  */
-function orderClassesByTitleAndLevel(collection) {}
+function orderClassesByTitleAndLevel(collection) {
+
+    collection.sort((classA, classB) => {
+      if (classA.title < classB.title) {
+        return -1;
+      } else if (classA.title > classB.title) {
+        return 1;
+      }
+
+      if (classA.level > classB.level) {
+        return -1;
+      } else if (classA.level < classB.level) {
+        return 1;
+      }
+      return 0;
+    });
+  
+
+    const sortedClasses = collection.map(({ title, instructor, level }) => ({
+      title,
+      instructor,
+      level,
+    }));
+  
+    return sortedClasses;
+  }
+  
+
+
 
 module.exports = {
   numberOfKeys,
